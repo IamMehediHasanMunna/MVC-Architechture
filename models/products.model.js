405984342a5036b9573ab75aa7dff20844a8b0af
Mehedi Hasan Munna <86718253+Mehed-Hasan-Munna@users.noreply.github.com>
2022-10-16 15:23:37 +0600

    MVC

---
+++ models/products.model.js
@@ -0,0 +1,12 @@
+const products = [
+  {
+    name: "Bike",
+    price: 570000,
+  },
+  {
+    name: "car",
+    price: 2400000,
+  },
+];
+
+module.exports = products;
---
+++ models/users.model.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    name: "munna",
+    age: 27,
+  },
+  {
+    name: "anika",
+    age: 24,
+  },
+];
+
+module.exports = users;
